c5c13ecc04a9d4dc7d347bbbd4960e6dd45cd75b
iqbalansyori <iqbal@icehousecorp.com>
2018-01-15 11:49:11 +0700

    Initial Commit

---
+++ DataStructurePractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructurePractice
+//
+//  Created by Iqbal Ansyori on 1/15/18.
+//  Copyright © 2018 Iqbal Ansyori. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
